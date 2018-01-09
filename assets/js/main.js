2b9dfe556fa10d2099bd1162637ddfd912a71891
Jonathan Zong <jonathan@jonathanzong.com>
2018-01-08 22:51:04 -0500

    interactive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-  $('.main-sections').css('margin-top', $(window).height() - 60);
+  var topmargin = $(window).height() - 60;
+  $('.main-sections').css('margin-top', topmargin);
+
+  $(window).resize(function() {
+    topmargin = $(window).height() - 60;
+    $('.main-sections').css('margin-top', topmargin);
+  });
 
   PDFObject.embed("/assets/pdf/WaveFanfare_2017_Snyder_score.pdf", "#score-pdf", {
     fallbackLink: "<p>The score for the performance can be viewed <a href='/assets/pdf/WaveFanfare_2017_Snyder_score.pdf'>by clicking here</a>.</p>"
@@ -16,50 +22,56 @@ $(document).ready(function() {
   });
 
   $('.down-arrow').click(function() {
-    $("html, body").animate({ scrollTop: $('.main-sections').css('margin-top') }, 500);
-  })
-});
-
-$(window).scroll(function() {
-
-  // selectors
-  var $window = $(window),
-      $body = $('.main-sections'),
-      $panel = $('.trigger-color');
-
-  // Change 33% earlier than scroll position so colour is there when you arrive.
-  var scroll = $window.scrollTop()// + ($window.height() / 3);
-
-  if (scroll > 10) {
-    $('.down-arrow').removeClass('show');
-  }
-  else {
-    $('.down-arrow').addClass('show');
-  }
-
-  $panel.each(function () {
-    var $this = $(this);
-
-    // if position is within range of this panel.
-    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-    // Remember we set the scroll to 33% earlier in scroll var.
-
-    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-
-      var targetClass = 'color-' + $(this).data('color');
-
-      if (!$body.hasClass(targetClass)) {
-        // Remove all classes on body with color-
-        $body.removeClass(function (index, css) {
-          return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
-        });
-
-        // Add class of currently active div
-        $body.addClass('color-' + $(this).data('color'));
-      }
-    }
+    $("html, body").animate({ scrollTop: topmargin }, 500);
   });
 
-}).scroll();
+  $(window).scroll(function() {
 
+    // selectors
+    var $window = $(window),
+        $body = $('.main-sections'),
+        $panel = $('.trigger-color');
 
+    // Change 33% earlier than scroll position so colour is there when you arrive.
+    var scroll = $window.scrollTop()// + ($window.height() / 3);
+
+    if (scroll > 10) {
+      $('.down-arrow').removeClass('show');
+    }
+    else {
+      $('.down-arrow').addClass('show');
+    }
+
+    if (scroll > topmargin) {
+      $('.interactive').addClass('active');
+    }
+    else {
+      $('.interactive').removeClass('active');
+    }
+
+    $panel.each(function () {
+      var $this = $(this);
+
+      // if position is within range of this panel.
+      // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+      // Remember we set the scroll to 33% earlier in scroll var.
+
+      if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+
+        var targetClass = 'color-' + $(this).data('color');
+
+        if (!$body.hasClass(targetClass)) {
+          // Remove all classes on body with color-
+          $body.removeClass(function (index, css) {
+            return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+          });
+
+          // Add class of currently active div
+          $body.addClass('color-' + $(this).data('color'));
+        }
+      }
+    });
+
+  }).scroll();
+
+});
